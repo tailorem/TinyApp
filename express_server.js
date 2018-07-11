46bf2226f4c29fce3cc39cb3e4ec09860724738f
Taylour <29444471+tailorem@users.noreply.github.com>
2018-07-11 04:25:46 +0000

    finished URL shortening part 1, implemented random string generator

--- express_server.js
+++ express_server.js
@@ -1,6 +1,8 @@
 // Require modules and set port
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 const PORT = 8080;
 
 // Set view engine to EJS
@@ -12,8 +14,21 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 }
 
+function generateRandomString() {
+  let chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  chars = chars.split("");
+  let result = "";
+  for (char in chars) {
+    result += chars[Math.floor(Math.random() * chars.length)];
+  }
+  return result.slice(0, 6);
+}
+generateRandomString();
+
+
 // On requests, say hello
 app.get("/", (req, res) => {
+console.log(generateRandomString());
   res.end("Home page is currently operational!");
 });
 
@@ -24,9 +39,19 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+// Create route handler for "urls_new"
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // view POST parameters
+  res.send("'Accepted'");
+});
+
 // Create route handler for "urls_show"
 app.get("/urls/:id", (req, res) => {
-  let templateVars = { shortURL: req.params.id, fullURL: urlDatabase[req.params.id] };
+  let templateVars = { shortURL: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
 });
 
